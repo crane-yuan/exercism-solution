2f9daffe8b1b15d4452dda728d19a3be3f2c0998
craneyuan <yuanxingnepu@gmail.com>
2018-03-31 17:13:04 +0800

    :zap: Update basic bash sketch

--- bash/basic.sh
+++ bash/basic.sh
@@ -7,15 +7,7 @@ main() {
 }
 
 func(){
-    input="$*"
-    reverse=""
-    len=${#input}
-    for (( i=len-1; i>=0; i-- ))
-    do
-        reverse="$reverse${input:$i:1}"
-        echo "$reverse"
-    done
-    echo "$reverse"
+
 }
 
 main "$@"
