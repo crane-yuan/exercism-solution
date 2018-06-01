6b7feb1a377409197e5685c1f5f27de94dbcbf45
craneyuan <yuanxingnepu@gmail.com>
2018-06-02 00:25:21 +0800

    :zap: Format code

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,6 +1,6 @@
 class Twofer {
     String twofer(String name) {
-        if(name != null && !name.isEmpty()) {
+        if (name != null && !name.isEmpty()) {
             return "One for" + name + ", one for me.";
         } else {
             return "One for you, one for me.";
