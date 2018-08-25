69c4edeeb2e05722d59099e37bcef5eefb380680
craneyuan <yuanxingnepu@gmail.com>
2018-08-25 15:28:28 +0800

    :sparkles: [java] Add Matrix solution

---
+++ java/matrix/src/main/java/Matrix.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Matrix {
+
+    private String matrix;
+    private List<int[]> rowList = new ArrayList<>();
+    private List<int[]> columnList = new ArrayList<>();
+
+    Matrix(String matrixAsString) {
+        this.matrix = matrixAsString;
+    }
+
+    int[] getRow(int rowNumber) {
+        if (this.rowList.isEmpty()) {
+            String[] line = matrix.split("\n");
+            for (String str : line) {
+                int[] row = Arrays.stream(str.split(" "))
+                                  .mapToInt(Integer::parseInt)
+                                  .toArray();
+                this.rowList.add(row);
+            }
+        }
+        return this.rowList.get(rowNumber);
+    }
+
+    int[] getColumn(int columnNumber) {
+        if (this.rowList.isEmpty()) {
+            getRow(0);
+        }
+        if (this.columnList.isEmpty()) {
+            int rowSize = this.rowList.get(0).length;
+            int columnSize = this.rowList.size();
+
+            for (int i = 0; i < rowSize; i++) {
+                int[] row = new int[columnSize];
+                for (int j = 0; j < columnSize; j++) {
+                    row[j] = this.rowList.get(j)[i];
+                }
+                this.columnList.add(row);
+            }
+        }
+        return this.columnList.get(columnNumber);
+    }
+}
