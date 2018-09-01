a2b250c13c1fce03693e37784207cf0c258f5ecf
craneyuan <yuanxingnepu@gmail.com>
2018-09-01 23:35:08 +0800

    :zap: [java] Improve ISBN Verifier solution

--- java/isbn-verifier/src/main/java/IsbnVerifier.java
+++ java/isbn-verifier/src/main/java/IsbnVerifier.java
@@ -2,29 +2,21 @@ class IsbnVerifier {
 
     boolean isValid(String stringToVerify) {
         boolean result = false;
-        String nums = stringToVerify.replaceAll("-", "");
+        String nums = stringToVerify.replaceAll("[^0-9X]", "");
         if (nums.length() != 10) {
             return result;
         }
 
-        for (int i = 0; i < 9; i++) {
-            if (!Character.isDigit(nums.charAt(i))) {
-                return result;
-            }
-        }
-
         int sum = 0;
         for (int i = 0; i < 9; i++) {
-            sum += (nums.charAt(i) - '0') * (10 - i);
+            sum += Character.getNumericValue(nums.charAt(i)) * (10 - i);
         }
 
         char last = nums.charAt(9);
-        if (last == 'x' || last == 'X') {
+        if (last == 'X') {
             sum += 10;
-        } else if (Character.isDigit(last)) {
-            sum += (last - '0');
         } else {
-            return result;
+            sum += Character.getNumericValue(last);
         }
 
         return sum % 11 == 0;
