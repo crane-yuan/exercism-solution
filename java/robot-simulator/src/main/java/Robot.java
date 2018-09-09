37da3f2d9e961bf7976459f047236de4cb033d98
craneyuan <yuanxingnepu@gmail.com>
2018-09-09 23:55:20 +0800

    :sparkles: [java] Add Robot Simulator solution

---
+++ java/robot-simulator/src/main/java/Robot.java
@@ -0,0 +1,86 @@
+public class Robot {
+
+    private Orientation orientation;
+    private GridPosition gridPosition;
+
+    public Robot(GridPosition initialGridPosition, Orientation initialOrientation) {
+        this.gridPosition = initialGridPosition;
+        this.orientation = initialOrientation;
+    }
+
+    public Orientation getOrientation() {
+        return orientation;
+    }
+
+    public GridPosition getGridPosition() {
+        return gridPosition;
+    }
+
+    public void turnRight() {
+        switch (orientation) {
+            case NORTH:
+                this.orientation = Orientation.EAST;
+                break;
+            case EAST:
+                this.orientation = Orientation.SOUTH;
+                break;
+            case SOUTH:
+                this.orientation = Orientation.WEST;
+                break;
+            case WEST:
+                this.orientation = Orientation.NORTH;
+                break;
+        }
+    }
+
+    public void turnLeft() {
+        switch (orientation) {
+            case NORTH:
+                this.orientation = Orientation.WEST;
+                break;
+            case EAST:
+                this.orientation = Orientation.NORTH;
+                break;
+            case SOUTH:
+                this.orientation = Orientation.EAST;
+                break;
+            case WEST:
+                this.orientation = Orientation.SOUTH;
+                break;
+        }
+    }
+
+    public void advance() {
+        switch (orientation) {
+            case NORTH:
+                this.gridPosition = new GridPosition(this.gridPosition.x, this.gridPosition.y + 1);
+                break;
+            case EAST:
+                this.gridPosition = new GridPosition(this.gridPosition.x + 1, this.gridPosition.y);
+                break;
+            case SOUTH:
+                this.gridPosition = new GridPosition(this.gridPosition.x, this.gridPosition.y - 1);
+                break;
+            case WEST:
+                this.gridPosition = new GridPosition(this.gridPosition.x - 1, this.gridPosition.y);
+                break;
+        }
+    }
+
+    public void simulate(String instructions) {
+        char[] array = instructions.toCharArray();
+        for (int i = 0; i < array.length; i++) {
+            switch (array[i]) {
+                case 'R':
+                    turnRight();
+                    break;
+                case 'L':
+                    turnLeft();
+                    break;
+                case 'A':
+                    advance();
+                    break;
+            }
+        }
+    }
+}
